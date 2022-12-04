c335ede6f4a9cf85d34eb42555967bae2c4d1c93
Smccb <sarahmmccb@gmail.com>
2022-12-04 20:19:30 +0000

    updates

--- Assets/Scripts/Chess/Pawn.cs
+++ Assets/Scripts/Chess/Pawn.cs
@@ -9,7 +9,7 @@ public class Pawn : Pieces
     // Start is called before the first frame update
     void Start()
     {
-        movedFromStartPos = false;
+       // movedFromStartPos = false;
     }
 
     // Update is called once per frame
